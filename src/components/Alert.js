a3cc49467771ec7bfc6f7df8644aed5c5e03bccb
Kishan <adesarakishu9101@gmail.com>
2024-12-11 23:09:05 +0530

    0 Words error fix

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,10 +6,12 @@ export default function Alert(props){
         return lower.charAt(0).toUpperCase() + lower.slice(1);
     }
     return(
-        props.alert && <div>
-            <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-                {capitalize(props.alert.type)}: {props.alert.message}
-            </div>
+        <div style={{height:"70px"}}>
+            {props.alert && <div>
+                <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+                    {capitalize(props.alert.type)}: {props.alert.message}
+                </div>
+            </div>}
         </div>
     );
 }
